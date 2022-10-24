6034d4014a8bf118852dae3a8643146dc9e8a5ed
Crazy2kcoder <109915780+chandrumurugasan@users.noreply.github.com>
2022-10-24 06:11:27 -0700

    Delete Assessments/Team leader (VASUKI)/Assignment-2 directory

--- Assessments/Team leader (VASUKI)/Assignment-2/dpsqlite.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('register.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE students (username TEXT, email TEXT, password TEXT)')
-print("Table created successfully")
-conn.close()
